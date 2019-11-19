ec06a9426dbb05cd514f4234a6aeb6a9cd87a995
Marcos-Coutinho <43754947+Marcos-Coutinho@users.noreply.github.com>
2019-11-19 00:58:29 -0400

    Delete flashlight-plugin-demo.js

--- www/js/flashlight-plugin-demo.js
+++
@@ -1,9 +0,0 @@
-function flashlightDemo() {
-  window.plugins.flashlight.available(function(isAvailable) {
-    if (isAvailable) {
-      window.plugins.flashlight.toggle();
-    } else {
-      alert("Flashlight not available on this device");
-    }
-  })
-}
